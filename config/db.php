1af3adad51f585e2db4b4c3ed646fcb1574b79a8
Celes <celestinomorenorodriguez@gmail.com>
2024-03-16 10:50:06 -0700

    Se modifico la base_url y los valore de la base de datos respecto al hosting

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 class Connect{
     public static function connect(){
         $host = "localhost";
-        $usuario = "root";
-        $password = "";
-        $database = "bd_escondida";
+        $usuario = "u393147787_celes";
+        $password = "Cls26tino";
+        $database = "u393147787_bd_escondida";
 
         $db = new mysqli($host,$usuario,$password,$database);
         $db->query("SET NAMES 'utf8'");
--- config/parameters.php
+++ config/parameters.php
@@ -1,5 +1,5 @@
 <?php
-define("base_url","http://192.168.1.118/proyectos/proyectoEscondida/");
+define("base_url","http://escondidasantasmarias.site/");
 define("controller_default", "clienteController");
 define("action_default","inicio");
 ?>
